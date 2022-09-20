4a19ec47e91b9fc5049301b7df9202fa6eba98e8
zitin2202 <zit2202@yandex.ru>
2022-09-20 23:47:35 +0300

    реализована система шахов

--- Classes/Field.cs
+++ Classes/Field.cs
@@ -17,7 +17,7 @@ namespace Classes
             {
              { new Rook(this,new Point(0,0), PlayerSide.Second), new Knight(this,new Point(0,1),PlayerSide.Second), new Bishop(this,new Point(0,2),PlayerSide.Second), new King(this,new Point(0,3),PlayerSide.Second), new Queen(this,new Point(0,4),PlayerSide.Second), new Bishop(this,new Point(0,5),PlayerSide.Second), new Knight(this,new Point(0,6),PlayerSide.Second), new Rook(this,new Point(0,7),PlayerSide.Second), },
              { new Pawn(this,new Point(1,0),PlayerSide.Second),  new Pawn(this,new Point(1,1),PlayerSide.Second),   new Pawn(this,new Point(1,2),PlayerSide.Second),   new Pawn(this,new Point(1,3),PlayerSide.Second), new Pawn(this,new Point(1,4),PlayerSide.Second),  new Pawn(this,new Point(1,5),PlayerSide.Second),   new Pawn(this,new Point(1,6),PlayerSide.Second),   new Pawn(this,new Point(1,7),PlayerSide.Second), },
-             { null,null,new Pawn(this,new Point(2,2),PlayerSide.First),null,null,null,null,null },
+             { null,null,null,null,null,null,null,null },
              { null,null,null,null,null,null,null,null },
              { null,null,null,null,null,null,null,null },
              { null,null,null,null,null,null,null,null },
--- Classes/Game.cs
+++ Classes/Game.cs
@@ -8,56 +8,132 @@ namespace Classes
 {
     public class Game
     {
-        private Field _f;
-        private (ChessPiece,TypeMove[,]) _activeChP = (null,new TypeMove[Field.maxY,Field.maxX]);
+        private Field _field;
+        private (ChessPiece, TypeMove[,]) _activeChP = (null, new TypeMove[Field.maxY, Field.maxX]);
         private IEnumerator _turn = TurnToGo();
         private bool _check = false;
         private Dictionary<ChessPiece, List<Point>> _protectKing;
         private List<List<Point>> _checkLines;
         private bool[,] _unsafeCell = new bool[Field.maxY, Field.maxX];
+        private string GUI="";
         public Game(Field f)
         {
-            _f = f;
+            _field = f;
         }
 
-        public bool Start(Point p)
+        public void Start()
         {
-            while
+            while (true)
+            {
+                _turn.MoveNext();
+                SecurityCheckAll();
+                _activeChP = (null, new TypeMove[Field.maxY, Field.maxX]);
+                ConsoleFieldGUI();
+                Console.WriteLine($"Очередь игрока: {_turn.Current}");
+                do
+                {
+                    Console.WriteLine("Выберите фигуру");
+                }
+                while (!Select(ConsoleInput()));
+                do
+                {
+                    Console.WriteLine("Выберите координаты");
+                }
+                while (!Action(ConsoleInput()));
+
+
+
+
+
+            }
+        }
+        public void ConsoleFieldGUI()
+        {
+            string cell = "|--|";
+            string fieldGui = "  ";
+
+            for (int x = 0; x < Field.maxX; x++)
+            {
+                fieldGui += $"{x}  ";
+            }
+            fieldGui += "\n";
+
+
+            for (int y = 0; y < Field.maxY; y++)
+            {
+
+                for (int x = 0; x < Field.maxX; x++)
+                {
+                    ChessPiece chP = _field.GetChP(new Point(y, x));
+
+                    fieldGui += $"{(x==0? $"{y} " : "")}{(chP == null ? "  " : chP.Side.ToString()[0].ToString() + chP.ChPType.ToString()[0].ToString())} ";
+
+
+
+                }
+                fieldGui += "\n";
+
+
+
+            }
+            Console.WriteLine(fieldGui);
+        }
+
+        public string StrMulti(string str, int mult) 
+        {
+            string multstr = "";
+            for (int i = 0; i <=mult; i++)
+            {
+                multstr += str;
+            }
+
+            return multstr;
+        }
+
+
+        public Point ConsoleInput()
+        {
+            string[] s =  Console.ReadLine().Split(',');
+            return new Point(int.Parse(s[0]), int.Parse(s[1]));
+
         }
 
         public bool Select(Point p)
         {
+            int count = 0;
+            ChessPiece chP = _field.GetChP(p);
+            if (!AccessChP(chP))
+            {
+                Console.WriteLine("Здесь нету фигуры, которую вы могли бы взять, выберите другую");
+                return false;
 
-            ChessPiece chP = _f.GetChP(p);
-            _turn.MoveNext();
-
-            //if (!AccessChP(chP))
-            //{
-            //    Console.WriteLine("Здесь нету фигуры, которую вы могли бы взять");
-            //    return false;
-            //}
-
-            //else
-            //{
-            //    Console.WriteLine($"Очередь игрока: {_turn.Current}");
-
-            //}
-
+            }
+          
 
             _activeChP.Item1 = chP;
 
             Console.WriteLine((chP.Side, chP.ChPType));
 
-            var list = chP.GetMoves();
+            EditMoves(chP,chP.GetMoves());
 
-            foreach (var line in list)
+            for (int y = 0; y<Field.maxY;y++)
             {
-                foreach (var i in line)
+                for (int x = 0; x <Field.maxY; x++)
                 {
-                    _activeChP.Item2[i.Item1.y, i.Item1.x] = i.Item2;
-                    ChessPiece targetChP = _f.GetChP(new Point(i.Item1.y, i.Item1.x));
-                    Console.WriteLine($"{(i.Item1.y, i.Item1.x)}: {((targetChP == null ? "Пусто" : targetChP.ChPType.ToString()), _activeChP.Item2[i.Item1.y, i.Item1.x])}");
+                    if (_activeChP.Item2[y, x] !=0 )
+                    {
+                        ChessPiece targetChP = _field.GetChP(new Point(y, x));
+                        Console.WriteLine($"{(y, x)}: {((targetChP == null ? "Пусто" : targetChP.ChPType.ToString()), _activeChP.Item2[y, x])}");
+                        count++;
+                    }
+
                 }
+
+            }
+            if (count==0)
+            {
+                Console.WriteLine("У этой фигуры нет возможных ходов");
+                return false;
 
             }
 
@@ -75,7 +151,7 @@ namespace Classes
 
             }
 
-            ChessPiece chP = _f.GetChP(p);
+            ChessPiece chP = _field.GetChP(p);
 
             switch (_activeChP.Item2[p.y, p.x])
             {
@@ -84,21 +160,67 @@ namespace Classes
                     break;
 
                 case TypeMove.Attack:
-                    Attack(_activeChP.Item1,_f.GetChP(p));
+                    Attack(_activeChP.Item1,p, chP);
                     break;
 
             }
-            _f.SetChP(_activeChP.Item1._p, null);
-            _f.SetChP(p, _activeChP.Item1);
+            _field.SetChP(_activeChP.Item1._p, null);
+            _field.SetChP(p, _activeChP.Item1);
 
-            _activeChP = (null, new TypeMove[Field.maxY, Field.maxX]);
 
             return true;
           
 
         }
 
-        private void EditMoves(ChessPiece thisChP,IEnumerable<IEnumerable<(Point, TypeMove)>> list, bool afterStop=false)
+        private void Move(ChessPiece myChP, Point targP)
+        {
+            Console.WriteLine($"{myChP.Side} {(myChP._p.y, myChP._p.x)}: {myChP.ChPType} идет на {(targP.y, targP.x)}");
+
+        }
+        private void Attack(ChessPiece myChP, Point targP, ChessPiece targChP)
+        {
+            Console.WriteLine($"{myChP.Side} {(myChP._p.y, myChP._p.x)}: {myChP.ChPType} съел {targChP.ChPType} на {(targP.y, targP.x)}");
+
+        }
+
+        private void EditMoves(ChessPiece thisChP, IEnumerable<IEnumerable<(Point, TypeMove)>> list)
+        {
+            foreach (var line in list)
+            {
+                foreach (var i in line)
+                {
+                    ChessPiece chP = _field.GetChP(i.Item1);
+                    Point p = i.Item1;
+                    TypeMove type = i.Item2;
+                    ChessPiece cellChP = _field.GetChP(p);
+                    TypeMove result = 0;
+
+
+                    if (cellChP == null && type != TypeMove.Attack) //на клетке нету фигуры и можно походить без атаки                     
+                            result = TypeMove.Simple;
+                    
+                    else if (cellChP != null) //на клетке есть фигура
+                    {
+                        if (thisChP.Side != cellChP.Side && type != TypeMove.Simple)//фигура вражеская и данным ходом можно атаковать
+                            result = TypeMove.Attack;
+                        else
+                            break;
+                    }
+
+                    if (AccessCell(p, thisChP))
+                        _activeChP.Item2[p.y, p.x] = result;
+
+                    if (result == TypeMove.Attack)
+                        break;                 
+
+                }
+
+            }
+
+        }
+
+        private void SecurityCheckChP(ChessPiece thisChP,IEnumerable<IEnumerable<(Point, TypeMove)>> list)
         {
             foreach (var line in list)
             {
@@ -109,30 +231,20 @@ namespace Classes
                 {
                     Point p = i.Item1;
                     TypeMove type = i.Item2;
-                    ChessPiece cellChP = _f.GetChP(p);
-                    TypeMove result = 0;
+                    ChessPiece cellChP = _field.GetChP(p);
 
                     if (cellChP == null && type != TypeMove.Attack) //все случаи, когда на клетке нету фигуры и можно походить без атаки
                     {
-
-                        if (afterStop) //если это пвседоход
+                        if (interval.Item1 == null)
                         {
-                            if (interval.Item1 == null)
-                            {
-                                _unsafeCell[p.y, p.x] = true;
+                            _unsafeCell[p.y, p.x] = true;
 
-                            }
-                            if (!check)
-                            {
-                                interval.Item2.Add(p);
-                            }
-                            continue;
                         }
-
-                        else 
-                            result = TypeMove.Simple;
-
-
+                        if (!check)
+                        {
+                            interval.Item2.Add(p);
+                        }
+                        continue;
                     }
 
                     else if (cellChP != null) //все случаи, когда на клетке есть фигура
@@ -141,21 +253,17 @@ namespace Classes
                         {
                             if (thisChP.Side != cellChP.Side) //фигура вражеская
                             {
-                                if (afterStop)
-                                {
-
                                     if (cellChP.ChPType == ChPType.King)
                                     {
-                                        if (interval.Item1 != null)
+                                        if (interval.Item1 != null)//между атакующей фигурой и королем есть еще одная фигура, защищающая короля
                                         {
                                             _protectKing[interval.Item1] = interval.Item2;
                                             break;
                                         }
-                                        else
+                                    else //непосредственная угроза королю
                                         {
-                                            _unsafeCell[p.y, p.x] = true;
-                                            _checkLines.Add(interval.Item2);
                                             check = true;
+                                            _checkLines.Add(interval.Item2);
                                         }
 
                                     }
@@ -165,16 +273,10 @@ namespace Classes
                                         interval.Item1 = cellChP;
                                     }
                                     else
-                                        break;
-                                }
-
-                                else
-                                    result = TypeMove.Attack;
-
-
+                                        break;                               
                             }
 
-                            else if (afterStop)
+                            else
                             {
                                 _unsafeCell[p.y, p.x] = true;
                                 break;
@@ -185,15 +287,6 @@ namespace Classes
                         else
                             break;
 
-
-                    }
-
-                    if (!afterStop)
-                    {
-                        _activeChP.Item2[p.y, p.x] = result;
-                        if (result == TypeMove.Attack)
-                            break;
-
                     }
 
                 }
@@ -202,19 +295,29 @@ namespace Classes
 
         }
 
-        private void Move(ChessPiece myChP, Point targP)
+        private void SecurityCheckAll()
         {
-            Console.WriteLine($"{myChP.Side} {(myChP._p.y, myChP._p.x)}: {myChP.ChPType} идет на {(targP.y, targP.x)}");
+            _protectKing = new Dictionary<ChessPiece, List<Point>>();
+            _checkLines = new List<List<Point>>();
+            _unsafeCell = new bool[Field.maxY, Field.maxX];
+            for (int y = 0; y < Field.maxY; y++)
+            {
+                for (int x = 0; x < Field.maxY; x++)
+                {
+                    ChessPiece chP = _field.GetChP(new Point(y, x));
+                    if (chP!=null && chP.Side != (PlayerSide)_turn.Current)
+                    {
+                        SecurityCheckChP(chP, chP.GetMoves());
+                    }
 
+                }
+
+            }
         }
-        private void Attack(ChessPiece myChP, ChessPiece targChP)
-        {
-            Console.WriteLine($"{myChP.Side} {(myChP._p.y, myChP._p.x)}: {myChP.ChPType} съел {targChP.ChPType} на {(targChP._p.y, targChP._p.x)}");
-
-        }
 
 
-        static private IEnumerator TurnToGo ()
+
+        static private IEnumerator<PlayerSide> TurnToGo ()
         {
             while (true)
             {
@@ -232,13 +335,8 @@ namespace Classes
 
             if ( chP != null && chP.Side == side)
             {
-                if(!_check)
-                    return true;
+                return true;
 
-                else
-                {
-
-                }
             }
 
             else
@@ -246,8 +344,56 @@ namespace Classes
                 return false;
             }
 
-            return true;
 
+        }
+
+        private bool AccessCell(Point p,ChessPiece chP)
+        {
+            bool access = true;
+            int lenCheckLines = (_checkLines != null ? _checkLines.Count : 0);
+
+
+            if (chP.ChPType == ChPType.King)
+            {
+                access = !_unsafeCell[p.y, p.x];              
+            }
+
+            else if (lenCheckLines < 2)
+            {
+                if (lenCheckLines == 1)
+                {
+                    access = InInterval(p, _checkLines[0]);
+                }
+
+                else if (_protectKing != null && _protectKing.ContainsKey(chP))
+                {
+                    access = InInterval(p, _protectKing[chP]);
+                }
+
+
+            }
+
+            else 
+                access = false;
+
+
+            return access;
+
+        }
+
+        private bool InInterval(Point p,List<Point> interval)
+        {
+            foreach (Point i in interval)
+            {
+                    if (p.y == i.y && p.x==i.x)
+                {
+                    return true;
+                }
+
+            }
+            return false;
+
+            
         }
 
     }
